47fd28521f6ec27f99bbb939545f74e8f470aa51
Aleks <absolutnik@gmail.com>
2014-12-28 11:40:02 -0500

    tests: Two programs modified to work with async zmq sockets.

--- loop_reqrep_client.py
+++ loop_reqrep_client.py
@@ -6,7 +6,6 @@ import sys
 
 import async_zmq
 
-port = "loop_reqrep"
 if len(sys.argv) > 1:
     port =  sys.argv[1]
 
@@ -14,24 +13,19 @@ counter = 0
 loop = asyncio.get_event_loop()
 def on_recv(socket, msg):
     global counter
-    print("Received reply:", msg)
-    if counter < 10:
-        socket.send("more requests".encode())
-        counter += 1
-    else:
-        loop.stop()
+    print("Received msg:", msg)
+#    if counter < 10:
+#        socket.send(b"more requests")
+#        counter += 1
+#    else:
+#        loop.stop()
 
 
-#context = zmq.Context()
-#print("Connecting to server...")
-#socket = context.socket(zmq.REQ)
-#socket.connect ("ipc:///tmp/%s" % port)
-
-zmq_sock = async_zmq.SocketFactory.req_socket("blabla",
+zmq_sock = async_zmq.SocketFactory.sub_socket("blabla",
                            loop=loop)
 zmq_sock.on_recv(functools.partial(on_recv, zmq_sock))
 
-zmq_sock.send ("Hello".encode())
+#zmq_sock.send ("Hello".encode())
 
 try:
     loop.run_forever()
--- loop_reqrep_server.py
+++ loop_reqrep_server.py
@@ -7,25 +7,24 @@ import sys
 
 import async_zmq
 
-port = "loop_reqrep"
+def publisher(sock, loop):
+    sock.send(b"I am alive!")
+    loop.call_later(1, publisher, sock, loop)
 
 if len(sys.argv) > 1:
     port =  sys.argv[1]
 
+loop = asyncio.get_event_loop()
 def on_recv(socket, msg):
     print("Received request:", msg)
-    socket.send("Back at ya!".encode())
+    loop.call_later(1, socket.send, ("Back at ya!".encode()))
 
-#context = zmq.Context()
-#socket = context.socket(zmq.REP)
-#socket.bind("ipc:///tmp/%s" % port)
-
-loop = asyncio.get_event_loop()
-zmq_sock = async_zmq.SocketFactory.rep_socket("blabla",
+zmq_sock = async_zmq.SocketFactory.pub_socket("blabla",
                            loop=loop)
-zmq_sock.on_recv(functools.partial(on_recv, zmq_sock))
+#zmq_sock.on_recv(functools.partial(on_recv, zmq_sock))
 
 try:
+    publisher(zmq_sock, loop)
     loop.run_forever()
 except (SystemExit, KeyboardInterrupt):
-    print("Exiting due to system interrup")
+    print("Exiting...")
